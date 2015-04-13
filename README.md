3faa3cb3156d4def2f94b5b3ab04ad89df14d3e3
Mikael Carpenter <mikaelcarp.dev@gmail.com>
2015-04-12 22:55:07 -0700

    added youtube news app

---
+++ README.md
@@ -0,0 +1,3 @@
+## React Examples
+
+Just various apps i've cloned to practice React.js and React-Native
---
+++ YoutubeNews/VideoWebView.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+	Text,
+	StyleSheet,
+	View,
+	WebView,
+} = React;
+
+var ViewVideo = React.createClass({
+	render: function() {
+		return (
+			<View style={styles.container}>
+				<WebView url={this.props.url}
+					renderLoading={this.renderLoading}
+					renderError={this.renderError} />
+			</View>
+		);
+	},
+	// renderLoading: function() {
+	// 	console.log('## webvew: renderloading()');
+	// },
+	// renderError: function() {
+	// 	console.log('## webvew: rendererror()');
+	// }
+});
+
+var styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#F6F6EF',
+		flexDirection: 'column'
+	}
+});
+
+module.exports = ViewVideo;
